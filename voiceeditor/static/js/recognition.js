1285a2b6aeca29fea373d6b69de6dc82e6ee68c2
Michal Hozza <mhozza@gmail.com>
2014-02-26 12:23:42 +0100

    Spracovanie commandov a mappingu, insertovanie na poziciu kurzora

--- voiceeditor/static/js/recognition.js
+++ voiceeditor/static/js/recognition.js
@@ -9,19 +9,24 @@ recognition.interimResults = true;
 recognition.onstart = function() {
     console.info('Recognition started');
 }
+
 recognition.onresult = function(event) {
     var interim_transcript = '';
     var final_transcript = '';
     for (var i = event.resultIndex; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
+            // for (var j in event.results[i]) {
+            //     console.log(j.toString() + ":" + event.results[i][j].transcript);
+            // }
             final_transcript += event.results[i][0].transcript;
         } else {
             interim_transcript += event.results[i][0].transcript;
         }
     }
     $('#voice_input').val(interim_transcript);
-    $('#editor').append(final_transcript);
+    process_input(final_transcript.trim().toLowerCase());
 };
+
 recognition.onerror = function(event) {
     if (event.error == 'no-speech') {
         console.log('info_no_speech');
@@ -79,8 +84,55 @@ function update_tables() {
     }).success(function(data) {
       set_commands(data);
     });
-    // console.log(window.mapping);
-    // console.log(window.features);
-    // console.log(window.commands);
+    console.log(window.mapping);
+    console.log(window.features);
+    console.log(window.commands);
     window.setTimeout("update_tables()", refresh_time);
+}
+
+function process_input(final_transcript) {
+    command = get_command(final_transcript);
+    console.log(command);
+    if (command == null) {
+        mapp = get_mapping(final_transcript)
+        console.log(mapp);
+        if (mapp == null) {
+            insert_text(final_transcript);
+        } else {
+            insert_text(mapp);
+        }
+    } else {
+        process_command(command);
+    }
+}
+
+function process_command(command) {
+    eval(command + "()");
+}
+
+function get_command(text) {
+    for(var i in window.commands) {
+        c = window.commands[i];
+        if (c.fields.words == text) {
+            return c.fields.command;
+        }
+    }
+    return null;
+}
+
+function get_mapping(text) {
+    for(var i in window.mapping) {
+        m = window.mapping[i];
+        if (m.fields.words == text) {
+            return m.fields.chars;
+        }
+    }
+    return null;
+}
+
+function insert_text(text) {
+    $('#editor').textrange('replace', text);
+    selection = $('#editor').textrange('get');
+    // console.log(selection);
+    $('#editor').textrange('setcursor', selection.end);
 }
